5d88e31ca400c177962336d0a90086a6f272bcd0
Harshwardhan9999 <harshwardhangalande9999@gmail.com>
2025-05-04 10:06:53 +0530

    added testimonials section with glow cards and updates the constants

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import LogoSection from './components/LogoSection';
 import FeatureCards from './sections/FeatureCards';
 import ExperienceSection from './sections/ExperienceSection';
 import TechStack from './sections/TechStack';
+import Testimonials from './sections/Testimonials';
+import Contact from './sections/Contact';
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
       <FeatureCards/>
       <ExperienceSection/>
       <TechStack/>
+      <Testimonials/>
     </>
   );
 }
--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -17,7 +17,7 @@ const GlowCard = ({ card, children, index }) => {
   }
 
   return (
-    <div ref={(el) => (cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)} className='card-border card timeline-card rounded-xl p-10'>
+    <div ref={(el) => (cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)} className='card-border card timeline-card rounded-xl p-10 mb-5'>
         <div className='glow' />
         <div className='flex items-center gap-1 mb-5'>
             {Array.from({length: 5}, (_, i) => (
